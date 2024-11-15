c408cdd2d9c9f2a17247e5522df3bc04f73dc0ae
ArmenManukyan12 <armenmanukyan80@gmail.com>
2024-11-15 12:35:27 +0400

    ok

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import Lesson4 from "./Lesson4/App";
-// import Lesson4 from "./Lesson5/App";
-import Lesson4 from "./Lesson6/App";
+import Lesson4 from "./Lesson5/App";
+// import Lesson4 from "./Lesson6/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Lesson4 />);
