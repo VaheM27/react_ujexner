ecc2ca67eac129096724e1043428fdf7c843c154
ArmenManukyan12 <armenmanukyan80@gmail.com>
2024-11-20 03:09:10 +0400

    ok

---
+++ src/Lesson7/components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="nav">
+      <ul>
+        <li>
+          <a href="#">Cars</a>
+        </li>
+        <li>
+          <a href="#">Clothes</a>
+        </li>
+        <li>
+          <a href="#">Food</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+export default Navbar;
