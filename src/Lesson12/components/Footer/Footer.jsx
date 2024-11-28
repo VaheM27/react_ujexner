e36ff660cfa72aa596a4b8393c4c9069f115597c
VaheM27 <vmnatsakanyan27@gmail.com>
2024-11-28 20:50:47 +0400

    ROUTES

--- src/Lesson12/components/Footer/Footer.jsx
+++ src/Lesson12/components/Footer/Footer.jsx
@@ -1,10 +1,14 @@
+import { NavLink } from "react-router-dom";
+import ROUTES from "../../Routes";
 import "./Footer.scss";
 
 const Footer = () => {
   return (
     <div className="footer">
       <ul className="footerUl">
-        <li>Home</li>
+        <li>
+          <NavLink to={ROUTES.HOME}>Home</NavLink>
+        </li>
         <li>Users</li>
         <li>Products</li>
         <li>Recipes</li>
