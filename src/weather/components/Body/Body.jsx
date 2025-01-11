27daa330638c9c2dcdd463692ef8340fac799646
VaheM27 <vmnatsakanyan27@gmail.com>
2025-01-11 21:33:25 +0400

    Minor changes

--- src/weather/components/Body/Body.jsx
+++ src/weather/components/Body/Body.jsx
@@ -16,125 +16,168 @@ const Body = ({ location, type }) => {
   };
 
   useEffect(() => {
-    axios({
-      baseURL: `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=ba8608127335c6068af01ea8e811dad7`,
-      method: "GET",
-    })
-      .then((res) => setData(res.data))
-      .catch(() => {
-        alert("This city doesn't exist.");
-      });
-
-    axios({
-      baseURL: `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=ba8608127335c6068af01ea8e811dad7`,
-      method: "GET",
-    }).then((res) => setWeeklyData(res.data));
+    const getWeather = async () => {
+      try {
+        await axios({
+          baseURL: `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=ba8608127335c6068af01ea8e811dad7`,
+          method: "GET",
+        }).then((res) => setData(res.data));
+        await axios({
+          baseURL: `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=ba8608127335c6068af01ea8e811dad7`,
+          method: "GET",
+        }).then((res) => setWeeklyData(res.data));
+      } catch (e) {
+        const error = e.response.data.message;
+        alert(error);
+      }
+    };
+    getWeather();
   }, [location]);
+  console.log(weeklyData);
+
+  const icon = `https://openweathermap.org/img/w/${data?.weather[0].icon}.png`;
 
   return (
     <div className="body">
-      <div className="today">
-        <p>{data?.name}</p>
-        {type === "celsius" ? (
-          <p>{Math.trunc(kelvinToCelsius(data?.main.temp || 0))}°C</p>
-        ) : (
-          <p>{Math.trunc(kelvinToFar(data?.main.temp || 0))}°F</p>
-        )}
-        <p>{data?.weather[0].main}</p>
-      </div>
-      <div className="weekly">
-        <div className="box">
-          <p>
-            {weeklyData?.list[0].dt_txt.split(" ")[0].replaceAll("-", "/")}:
-          </p>
-          {type === "celsius" ? (
-            <p>
-              {Math.trunc(kelvinToCelsius(weeklyData?.list[0].main.temp || 0))}
-              °C
+      {data !== null && weeklyData !== null && (
+        <div>
+          <div className="today">
+            <p>{data.name}</p>
+            {type === "celsius" ? (
+              <p>{Math.trunc(kelvinToCelsius(data.main.temp || 0))}°C</p>
+            ) : (
+              <p>{Math.trunc(kelvinToFar(data.main.temp || 0))}°F</p>
+            )}
+            <p
+              style={{
+                display: "flex",
+                alignItems: "center",
+                columnGap: "20px",
+              }}
+            >
+              {data.weather[0].main}
+              <img src={icon} alt="" />
             </p>
-          ) : (
-            <p>
-              {Math.trunc(kelvinToFar(weeklyData?.list[0].main.temp || 0))}°F
-            </p>
-          )}
-        </div>
+          </div>
+          <div className="weekly">
+            <div className="box">
+              <p>
+                {weeklyData.list[0].dt_txt.split(" ")[0].replaceAll("-", "/")}:
+              </p>
+              {type === "celsius" ? (
+                <p>
+                  {Math.trunc(
+                    kelvinToCelsius(weeklyData.list[0].main.temp || 0)
+                  )}
+                  °C
+                </p>
+              ) : (
+                <p>
+                  {Math.trunc(kelvinToFar(weeklyData.list[0].main.temp || 0))}
+                  °F
+                </p>
+              )}
+              <img src={icon} alt="" />
+            </div>
 
-        <div className="box">
-          <p>
-            {weeklyData?.list[3].dt_txt.split(" ")[0].replaceAll("-", "/")}:
-          </p>
-          {type === "celsius" ? (
-            <p>
-              {Math.trunc(kelvinToCelsius(weeklyData?.list[3].main.temp || 0))}
-              °C
-            </p>
-          ) : (
-            <p>
-              {Math.trunc(kelvinToFar(weeklyData?.list[3].main.temp || 0))}°F
-            </p>
-          )}
+            <div className="box">
+              <p>
+                {weeklyData.list[3].dt_txt.split(" ")[0].replaceAll("-", "/")}:
+              </p>
+              {type === "celsius" ? (
+                <p>
+                  {Math.trunc(
+                    kelvinToCelsius(weeklyData.list[3].main.temp || 0)
+                  )}
+                  °C
+                </p>
+              ) : (
+                <p>
+                  {Math.trunc(kelvinToFar(weeklyData.list[3].main.temp || 0))}
+                  °F
+                </p>
+              )}
+              <img src={icon} alt="" />
+            </div>
+            <div className="box">
+              <p>
+                {weeklyData.list[11].dt_txt.split(" ")[0].replaceAll("-", "/")}:
+              </p>
+              {type === "celsius" ? (
+                <p>
+                  {Math.trunc(
+                    kelvinToCelsius(weeklyData.list[11].main.temp || 0)
+                  )}
+                  °C
+                </p>
+              ) : (
+                <p>
+                  {Math.trunc(kelvinToFar(weeklyData.list[11].main.temp || 0))}
+                  °F
+                </p>
+              )}
+              <img src={icon} alt="" />
+            </div>
+            <div className="box">
+              <p>
+                {weeklyData.list[19].dt_txt.split(" ")[0].replaceAll("-", "/")}:
+              </p>
+              {type === "celsius" ? (
+                <p>
+                  {Math.trunc(
+                    kelvinToCelsius(weeklyData.list[19].main.temp || 0)
+                  )}
+                  °C
+                </p>
+              ) : (
+                <p>
+                  {Math.trunc(kelvinToFar(weeklyData.list[19].main.temp || 0))}
+                  °F
+                </p>
+              )}
+              <img src={icon} alt="" />
+            </div>
+            <div className="box">
+              <p>
+                {weeklyData.list[27].dt_txt.split(" ")[0].replaceAll("-", "/")}:
+              </p>
+              {type === "celsius" ? (
+                <p>
+                  {Math.trunc(
+                    kelvinToCelsius(weeklyData.list[27].main.temp || 0)
+                  )}
+                  °C
+                </p>
+              ) : (
+                <p>
+                  {Math.trunc(kelvinToFar(weeklyData.list[27].main.temp || 0))}
+                  °F
+                </p>
+              )}
+              <img src={icon} alt="" />
+            </div>
+            <div className="box">
+              <p>
+                {weeklyData.list[35].dt_txt.split(" ")[0].replaceAll("-", "/")}:
+              </p>
+              {type === "celsius" ? (
+                <p>
+                  {Math.trunc(
+                    kelvinToCelsius(weeklyData.list[35].main.temp || 0)
+                  )}
+                  °C
+                </p>
+              ) : (
+                <p>
+                  {Math.trunc(kelvinToFar(weeklyData.list[35].main.temp || 0))}
+                  °F
+                </p>
+              )}
+              <img src={icon} alt="" />
+            </div>
+          </div>
         </div>
-        <div className="box">
-          <p>
-            {weeklyData?.list[11].dt_txt.split(" ")[0].replaceAll("-", "/")}:
-          </p>
-          {type === "celsius" ? (
-            <p>
-              {Math.trunc(kelvinToCelsius(weeklyData?.list[11].main.temp || 0))}
-              °C
-            </p>
-          ) : (
-            <p>
-              {Math.trunc(kelvinToFar(weeklyData?.list[11].main.temp || 0))}°F
-            </p>
-          )}
-        </div>
-        <div className="box">
-          <p>
-            {weeklyData?.list[19].dt_txt.split(" ")[0].replaceAll("-", "/")}:
-          </p>
-          {type === "celsius" ? (
-            <p>
-              {Math.trunc(kelvinToCelsius(weeklyData?.list[19].main.temp || 0))}
-              °C
-            </p>
-          ) : (
-            <p>
-              {Math.trunc(kelvinToFar(weeklyData?.list[19].main.temp || 0))}°F
-            </p>
-          )}
-        </div>
-        <div className="box">
-          <p>
-            {weeklyData?.list[27].dt_txt.split(" ")[0].replaceAll("-", "/")}:
-          </p>
-          {type === "celsius" ? (
-            <p>
-              {Math.trunc(kelvinToCelsius(weeklyData?.list[27].main.temp || 0))}
-              °C
-            </p>
-          ) : (
-            <p>
-              {Math.trunc(kelvinToFar(weeklyData?.list[27].main.temp || 0))}°F
-            </p>
-          )}
-        </div>
-        <div className="box">
-          <p>
-            {weeklyData?.list[35].dt_txt.split(" ")[0].replaceAll("-", "/")}:
-          </p>
-          {type === "celsius" ? (
-            <p>
-              {Math.trunc(kelvinToCelsius(weeklyData?.list[35].main.temp || 0))}
-              °C
-            </p>
-          ) : (
-            <p>
-              {Math.trunc(kelvinToFar(weeklyData?.list[35].main.temp || 0))}°F
-            </p>
-          )}
-        </div>
-      </div>
+      )}
     </div>
   );
 };
