d1adc628f973a371d8e03de12e9494c487266a58
Davit <davit.petrosyan10@edu.ysu.am>
2025-01-11 17:20:09 +0400

    WP

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "../src/Weather/App";
+import App from "./WeatherProject/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
