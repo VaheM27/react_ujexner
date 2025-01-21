ccf94448c851431446ed506e904d2e76d372f9b3
Davit <davit.petrosyan10@edu.ysu.am>
2025-01-21 20:47:42 +0400

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import Button from "./components/Button/Button";
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <Button />
+      <p>App</p>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./Lesson29/App";
-import { BrowserRouter } from "react-router-dom";
+import App from "./Lesson30/App";
+// import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./Lesson29/store/store";
 
