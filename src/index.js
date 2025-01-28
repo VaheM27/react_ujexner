642bb4eb7e1af7d8c2c8ce46c39251377c652b8d
Davit <davit.petrosyan10@edu.ysu.am>
2025-01-28 16:05:33 +0400

    D

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./WeatherProject/App";
+import App from "./ReduxTask/App";
+import { BrowserRouter } from "react-router";
+
+import { Provider } from "redux";
+import store from "./ReduxTask/components/Store/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
