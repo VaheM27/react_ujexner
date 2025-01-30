37dc80e40e927a1922244787d651bb6b2c0f2b92
ArmenManukyan12 <armenmanukyan80@gmail.com>
2025-01-30 13:12:51 +0400

    ok

--- src/Task/components/Weather/Weather.jsx
+++ src/Task/components/Weather/Weather.jsx
@@ -4,21 +4,21 @@ import instance from "../../axios";
 import "./Weather.css";
 
 const Weather = () => {
-    const [posts, setPosts] = useState([]);
-    const [url, setUrl] = useState("posts");
-    useEffect(() => {
-        instance({
-            url: url,
-            method: "GET",
-            params: {
-            _limit: 10,
-            _start: 30,
-            },
-        }).then((res) => setPosts(res.data));
-        instance({
-            method: "DELETE",
-            url: url + "/1",
-        });
-    }, [url]);
+    // const [posts, setPosts] = useState([]);
+    // const [url, setUrl] = useState("posts");
+    // useEffect(() => {
+    //     instance({
+    //         url: url,
+    //         method: "GET",
+    //         params: {
+    //         _limit: 10,
+    //         _start: 30,
+    //         },
+    //     }).then((res) => setPosts(res.data));
+    //     instance({
+    //         method: "DELETE",
+    //         url: url + "/1",
+    //     });
+    // }, [url]);
 }
 export default Weather;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./Lesson33/App";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./Lesson33/store/store";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
